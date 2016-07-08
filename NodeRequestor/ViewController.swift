ade9e14a3d78366c2df960807e831aa8e786ece1
Eugene Golovanov <eugene.golovanov@hart.com>
2016-07-08 01:57:14 -0700

    first commit

--- NodeRequestor/ViewController.swift
+++ NodeRequestor/ViewController.swift
@@ -12,14 +12,55 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+
+    @IBAction func requestAction(sender: UIButton) {
+        
+        let urlPath: String = "https://egtodo.herokuapp.com/todos"
+        let url = NSURL(string: urlPath)
+        let request = NSMutableURLRequest(URL: url!);
+        request.HTTPMethod = "GET"
+        
+        let task = NSURLSession.sharedSession().dataTaskWithRequest(request) {
+            (data:NSData?, response:NSURLResponse?, error:NSError?) in
+            
+            //in case of error
+            if error != nil {
+                print("shit err")
+                return
+            } else {
+                guard let data = data else {print("error getting data"); return}
+                do {
+                    let json = try NSJSONSerialization.JSONObjectWithData(data, options: .AllowFragments)
+//                    print("JSON:\(json)")
+                    
+
+                    //output of json is array
+                    for todo in json as! [AnyObject] {
+                        if let desc = todo["description"] as? String {
+                            print("DESCRIPT: \(desc)")
+                        }
+                    }
+
+                    
+                    
+                } catch {
+                    print("ERROR:\(error)")
+                }
+            }
+        }
+        task.resume();
     }
 
+    
+    
+    
+    
+    
+    
+    
 
 }
 
